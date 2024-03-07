12f67352fe23d3cd1f79f3e0a2ca53e1ff5a9344
unknown <139689090+resforx@users.noreply.github.com>
2024-03-07 23:49:40 +0900

    initial commit

---
+++ rust_test/target/debug/deps/rust_test-937890a028e3657a.d
@@ -0,0 +1,5 @@
+D:\GDFsector\rustall\rust_test\target\debug\deps\librust_test-937890a028e3657a.rmeta: src\main.rs
+
+D:\GDFsector\rustall\rust_test\target\debug\deps\rust_test-937890a028e3657a.d: src\main.rs
+
+src\main.rs:
---
+++ rust_test/target/debug/deps/rust_test-992ce9d91edd2e00.d
@@ -0,0 +1,5 @@
+D:\GDFsector\rustall\rust_test\target\debug\deps\librust_test-992ce9d91edd2e00.rmeta: src\main.rs
+
+D:\GDFsector\rustall\rust_test\target\debug\deps\rust_test-992ce9d91edd2e00.d: src\main.rs
+
+src\main.rs:
